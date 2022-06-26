889cac36c392082f684ac2d119b706467638fdf0
Muuhammad Arsil Khan <arsil.36k@gmail.com>
2022-06-26 17:53:49 +0500

    Renergy-Api-Gateway initial commit

---
+++ Renergy-API-Gateway/src/test/java/com/renergy/gateway/RenergyApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renergy.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenergyApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
